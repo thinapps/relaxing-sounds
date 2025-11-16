57942f956ab3d996ca6e0aea1eba5750c59f333e
Jesse Nickles <jesse@littlebizzy.com>
2025-11-16 14:30:30 +0700

    Update MainActivity.kt

--- app/src/main/java/top/thinapps/relaxingsounds/ui/MainActivity.kt
+++ app/src/main/java/top/thinapps/relaxingsounds/ui/MainActivity.kt
@@ -1,8 +1,8 @@
-package top.thinapps.relaxingsounds
+package top.thinapps.relaxingsounds.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import top.thinapps.relaxingsounds.R
+import top.thinapps.relaxingsounds.R // NOTE: You may need to change this import as well.
 
 class MainActivity : AppCompatActivity() {
 
