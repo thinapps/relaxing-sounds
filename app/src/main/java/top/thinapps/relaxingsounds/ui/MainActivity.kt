3cd9f7cb48aebf60db9c8d6ba891900aee7e369c
Jesse Nickles <jesse@littlebizzy.com>
2025-11-16 11:40:17 +0700

    Create MainActivity.kt

---
+++ app/src/main/java/top/thinapps/relaxingsounds/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package top.thinapps.relaxingsounds
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import top.thinapps.relaxingsounds.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
